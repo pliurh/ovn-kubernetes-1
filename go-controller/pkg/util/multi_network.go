ovn-org/ovn-kubernetes

UDN gw: use network name as VRF name for cluster UDNs
Anticipating that these VRF names are going to be exposed through BGP,
we should to use friendlier names for our VRFs. The most natural name to
use is the network name. Thus giving a cluster UDN a name below 15
characters that matches an already existing VRF not managed by ovn-k
will fail. This is considered an admin problem and not an ovn-k problem
for now.

Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

go-controller/pkg/util/multi_network.go
@@ -1026,3 +1026,27 @@ func AllowsPersistentIPs(netInfo NetInfo) bool {
 		return false
 	}
 }
+
+// GetNetworkVRFNameForBGP returns a valid VFR name for the network to use over
+// BGP. Returns "default" for the default network. In case a valid name is not
+// found, returns an empty string
+func GetNetworkVRFNameForBGP(netInfo BasicNetInfo) string {
+	if netInfo.GetNetworkName() == types.DefaultNetworkName {
+		return types.DefaultNetworkName
+	}
+	// only cluster UDN networks with names under 15 char are valid for BGP
+	vrfDeviceName := strings.TrimPrefix(netInfo.GetNetworkName(), "cluster.udn.")
+	if len(vrfDeviceName) > 15 || vrfDeviceName == netInfo.GetNetworkName() || netInfo.GetNetworkName() == types.DefaultNetworkName {
+		return ""
+	}
+	return vrfDeviceName
+}
+
+func GetNetworkVRFNameForDevice(netInfo BasicNetInfo, networkID int) string {
+	// try a valid BGP VRF name first
+	vrfDeviceName := GetNetworkVRFNameForBGP(netInfo)
+	if vrfDeviceName == "" {
+		vrfDeviceName = fmt.Sprintf("%s%d%s", types.UDNVRFDevicePrefix, networkID, types.UDNVRFDeviceSuffix)
+	}
+	return vrfDeviceName
+}

pliurh/ovn-kubernetes-1

go-controller/pkg/util/multi_network.go
@@ -1334,3 +1334,14 @@ func AllowsPersistentIPs(netInfo NetInfo) bool {
 func IsPodNetworkAdvertisedAtNode(netInfo NetInfo, node string) bool {
 	return len(netInfo.GetPodNetworkAdvertisedOnNodeVRFs(node)) > 0
 }
+
+func GetNetworkVRFName(netInfo NetInfo) string {
+	if netInfo.GetNetworkName() == types.DefaultNetworkName {
+		return types.DefaultNetworkName
+	}
+	vrfDeviceName := strings.TrimPrefix(netInfo.GetNetworkName(), "cluster.udn.")
+	if len(vrfDeviceName) < 16 && vrfDeviceName != netInfo.GetNetworkName() && vrfDeviceName != types.DefaultNetworkName {
+		return vrfDeviceName
+	}
+	return fmt.Sprintf("%s%d%s", types.UDNVRFDevicePrefix, netInfo.GetNetworkID(), types.UDNVRFDeviceSuffix)
+}