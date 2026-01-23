ovn-kubernetes/ovn-kubernetes

no-overlay: add toReceive routes for pod subnets to FrrConfiguration
In no-overlay mode, the cluster needs to accept incoming routes to the
pod subnets from BGP neighbors. This change enhances the
RouteAdvertisements controller to automatically populate the 'ToReceive'
field of BGP neighbors with the network's pod subnets when no-overlay
transport is used.

- Add unit tests for the new functionality.

Signed-off-by: Peng Liu <pliu@redhat.com>

go-controller/pkg/clustermanager/routeadvertisements/controller.go
@@ -324,6 +324,8 @@ type selectedNetworks struct {
 	prefixLength map[string]uint32
 	// networkType is a map of selected network to their topology
 	networkTopology map[string]string
+	// networkTransport is a map of selected network to their transport mode
+	networkTransport map[string]string
 }
 
 // generateFRRConfigurations generates FRRConfigurations for the route
@@ -351,10 +353,11 @@ func (c *Controller) generateFRRConfigurations(ra *ratypes.RouteAdvertisements) 
 	// validate and gather information about the networks
 	networkSet := sets.New[string]()
 	selectedNetworks := &selectedNetworks{
-		networkVRFs:     map[string]string{},
-		networkSubnets:  map[string][]string{},
-		prefixLength:    map[string]uint32{},
-		networkTopology: map[string]string{},
+		networkVRFs:      map[string]string{},
+		networkSubnets:   map[string][]string{},
+		prefixLength:     map[string]uint32{},
+		networkTopology:  map[string]string{},
+		networkTransport: map[string]string{},
 	}
 	for _, nad := range nads {
 		networkName := util.GetAnnotatedNetworkName(nad)
@@ -385,6 +388,7 @@ func (c *Controller) generateFRRConfigurations(ra *ratypes.RouteAdvertisements) 
 		selectedNetworks.vrfs = append(selectedNetworks.vrfs, vrf)
 		selectedNetworks.networkVRFs[vrf] = networkName
 		selectedNetworks.networkTopology[networkName] = network.TopologyType()
+		selectedNetworks.networkTransport[networkName] = network.GetNetworkTransport()
 		// TODO check overlaps?
 		for _, cidr := range network.Subnets() {
 			subnet := cidr.CIDR.String()
@@ -670,6 +674,32 @@ func (c *Controller) generateFRRConfiguration(
 					Prefixes: advertisePrefixes,
 				},
 			}
+
+			// For no-overlay networks, add routes to pod subnets to the accepted routes list
+			if selectedNetworks.networkTransport[matchedNetwork] == config.TransportNoOverlay {
+				// Get the pod subnets for this network (the network subnets, not host subnets)
+				podSubnets := selectedNetworks.networkSubnets[matchedNetwork]
+				if len(podSubnets) > 0 {
+					// Filter pod subnets by IP family to match the neighbor
+					filteredPodSubnets := util.MatchAllIPNetsStringFamily(isIPV6, podSubnets)
+					if len(filteredPodSubnets) > 0 {
+						neighbor.ToReceive = frrtypes.Receive{
+							Allowed: frrtypes.AllowedInPrefixes{
+								Mode:     frrtypes.AllowRestricted,
+								Prefixes: neighbor.ToReceive.Allowed.Prefixes,
+							},
+						}
+						for _, subnet := range filteredPodSubnets {
+							neighbor.ToReceive.Allowed.Prefixes = append(neighbor.ToReceive.Allowed.Prefixes, frrtypes.PrefixSelector{
+								Prefix: subnet,
+								LE:     selectedNetworks.prefixLength[subnet],
+								GE:     selectedNetworks.prefixLength[subnet],
+							})
+						}
+					}
+				}
+			}
+
 			targetRouter.Neighbors = append(targetRouter.Neighbors, neighbor)
 		}
 		if len(targetRouter.Neighbors) == 0 {

pliurh/ovn-kubernetes-1

go-controller/pkg/clustermanager/routeadvertisements/controller.go
@@ -324,6 +324,8 @@ type selectedNetworks struct {
 	prefixLength map[string]uint32
 	// networkType is a map of selected network to their topology
 	networkTopology map[string]string
+	// networkTransport is a map of selected network to their transport mode
+	networkTransport map[string]string
 }
 
 // generateFRRConfigurations generates FRRConfigurations for the route
@@ -351,10 +353,11 @@ func (c *Controller) generateFRRConfigurations(ra *ratypes.RouteAdvertisements) 
 	// validate and gather information about the networks
 	networkSet := sets.New[string]()
 	selectedNetworks := &selectedNetworks{
-		networkVRFs:     map[string]string{},
-		networkSubnets:  map[string][]string{},
-		prefixLength:    map[string]uint32{},
-		networkTopology: map[string]string{},
+		networkVRFs:      map[string]string{},
+		networkSubnets:   map[string][]string{},
+		prefixLength:     map[string]uint32{},
+		networkTopology:  map[string]string{},
+		networkTransport: map[string]string{},
 	}
 	for _, nad := range nads {
 		networkName := util.GetAnnotatedNetworkName(nad)
@@ -385,6 +388,7 @@ func (c *Controller) generateFRRConfigurations(ra *ratypes.RouteAdvertisements) 
 		selectedNetworks.vrfs = append(selectedNetworks.vrfs, vrf)
 		selectedNetworks.networkVRFs[vrf] = networkName
 		selectedNetworks.networkTopology[networkName] = network.TopologyType()
+		selectedNetworks.networkTransport[networkName] = network.GetNetworkTransport()
 		// TODO check overlaps?
 		for _, cidr := range network.Subnets() {
 			subnet := cidr.CIDR.String()
@@ -670,6 +674,31 @@ func (c *Controller) generateFRRConfiguration(
 					Prefixes: advertisePrefixes,
 				},
 			}
+
+			// For no-overlay networks, add routes to pod subnets to the accepted routes list
+			if selectedNetworks.networkTransport[matchedNetwork] == types.NetworkTransportNoOverlay {
+				// Get the pod subnets for this network (the network subnets, not host subnets)
+				podSubnets := selectedNetworks.networkSubnets[matchedNetwork]
+				if len(podSubnets) > 0 {
+					// Filter pod subnets by IP family to match the neighbor
+					filteredPodSubnets := util.MatchAllIPNetsStringFamily(isIPV6, podSubnets)
+					if len(filteredPodSubnets) > 0 {
+						neighbor.ToReceive = frrtypes.Receive{
+							Allowed: frrtypes.AllowedInPrefixes{
+								Mode: frrtypes.AllowRestricted,
+							},
+						}
+						for _, subnet := range filteredPodSubnets {
+							neighbor.ToReceive.Allowed.Prefixes = append(neighbor.ToReceive.Allowed.Prefixes, frrtypes.PrefixSelector{
+								Prefix: subnet,
+								LE:     selectedNetworks.prefixLength[subnet],
+								GE:     selectedNetworks.prefixLength[subnet],
+							})
+						}
+					}
+				}
+			}
+
 			targetRouter.Neighbors = append(targetRouter.Neighbors, neighbor)
 		}
 		if len(targetRouter.Neighbors) == 0 {